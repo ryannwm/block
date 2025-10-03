860c6238e132e1130246f77f3abae7b00e168c4e
Adryan <adryanadzanisyafuan@gmail.com>
2025-10-03 20:18:56 +0700

    modify migration, changing userId on table session to user_id

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -30,13 +30,13 @@ return new class extends Migration
 
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
-            $table->unsignedBigInteger('userId')->nullable()->index(); 
+            $table->unsignedBigInteger('user_id')->nullable()->index(); 
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->longText('payload');
             $table->integer('last_activity')->index();
 
-            $table->foreign('userId')->references('userId')->on('users')->onDelete('cascade');
+            $table->foreign('user_Id')->references('userId')->on('users')->onDelete('cascade');
         });
     }
 
