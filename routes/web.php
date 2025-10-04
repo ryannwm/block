aa5d20bfba873a04bf1723846f067672730e966a
Adryan <adryanadzanisyafuan@gmail.com>
2025-10-04 10:39:02 +0700

    add middleware and modifying dashboard view

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,32 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Middleware\CheckUserType;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function(){
-    return view('dashboard');
-})
-->middleware('auth')
--> name('dashboard');
 
 Route::get('/login', [AuthController::class, 'showLogin'])
 ->name('login');
 Route::post('/login', [AuthController::class, 'login'])
-    ->name('login.attempt');
+->name('login.attempt');
 
 Route::post('/logout', [AuthController::class, 'logout'])
-    ->middleware('auth')
-    ->name('logout');
+->middleware('auth')
+->name('logout');
 
 Route::get('/register', [AuthController::class, 'showRegister'])
-    ->name('register');
+->name('register');
 
 Route::post('/register', [AuthController::class, 'register'])
-    ->name('register.post');
+->name('register.post');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})
+->middleware(['auth', 'checkUserType:admin,user'])
+->name('dashboard');
+
