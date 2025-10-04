a891b090f34b75781a78a837f04b8e3834521e91
Adryan <adryanadzanisyafuan@gmail.com>
2025-10-04 11:43:35 +0700

    adding edit profile view and modifying logout display

--- resources/views/components/partials/navbar.blade.php
+++ resources/views/components/partials/navbar.blade.php
@@ -1,5 +1,5 @@
 <div class="flex h-14 max-w-full w-full items-center gap-2 px-4">
-    <button type="button" class="btn-ghost" onclick="document.dispatchEvent(new CustomEvent('basecoat:sidebar'))"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-panel-left-icon lucide-panel-left"><rect width="18" height="18" x="3" y="3" rx="2"/><path d="M9 3v18"/></svg></button>
+    <button type="button" data-tooltip="Toggle sidebar" data-side="bottom" data-align="start" class="btn-ghost" onclick="document.dispatchEvent(new CustomEvent('basecoat:sidebar'))"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-panel-left-icon lucide-panel-left"><rect width="18" height="18" x="3" y="3" rx="2"/><path d="M9 3v18"/></svg></button>
     @auth
     <div class="mr-auto flex items-center gap-1">
         <p class="text">Welcome,</p> 
@@ -17,9 +17,27 @@
     <span class="block dark:hidden"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" /></svg></span>
     </button>
     @auth
-    <form method="POST" action="{{ route('logout') }}">
-        @csrf
-        <button type="submit" class="btn h-8">Logout</button>
-    </form>
+    <div id="demo-dropdown-menu" class="dropdown-menu">
+        <button type="button" data-tooltip="Profile"
+    data-side="bottom" data-align="end" id="demo-dropdown-menu-trigger" aria-haspopup="menu" aria-controls="demo-dropdown-menu-menu" aria-expanded="false" class="btn-icon-primary size-8"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-icon lucide-user"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg></button>
+        <div id="demo-dropdown-menu-popover" data-popover data-side="bottom" data-align="end" aria-hidden="true" class="min-w-56">
+          <div role="menu" id="demo-dropdown-menu-menu" aria-labelledby="demo-dropdown-menu-trigger">
+            <div role="group" aria-labelledby="account-options">
+              <div role="heading" id="account-options">Account</div>
+              <div role="menuitem" onclick="document.getElementById('profile-dialog').showModal()">
+                Profile
+                <span class="text-muted-foreground ml-auto text-xs tracking-widest"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-round-cog-icon lucide-user-round-cog"><path d="m14.305 19.53.923-.382"/><path d="m15.228 16.852-.923-.383"/><path d="m16.852 15.228-.383-.923"/><path d="m16.852 20.772-.383.924"/><path d="m19.148 15.228.383-.923"/><path d="m19.53 21.696-.382-.924"/><path d="M2 21a8 8 0 0 1 10.434-7.62"/><path d="m20.772 16.852.924-.383"/><path d="m20.772 19.148.924.383"/><circle cx="10" cy="8" r="5"/><circle cx="18" cy="18" r="3"/></svg></span>
+              </div>
+            </div>
+            <hr role="separator" />
+            <div role="menuitem" onclick="document.getElementById('logout-dialog').showModal()">
+              Logout
+              <span class="text-muted-foreground ml-auto text-xs tracking-widest"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-log-out-icon lucide-log-out"><path d="m16 17 5-5-5-5"/><path d="M21 12H9"/><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"/></svg></span>
+            </div>
+          </div>
+        </div>
+    </div>
+
+
     @endauth
 </div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,4 +4,67 @@
 
 @section('content')
     @include('components.partials.'.auth()->user()->userType)
+    <dialog id="logout-dialog" class="dialog" aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
+        <article>
+            <header>
+                <h2 id="logout-dialog-title">Are you sure you want to log out?</h2>
+                <p id="logout-dialog-description">You will need to log in again to access your account.</p>
+            </header>
+
+            <footer>
+                <button class="btn-outline" onclick="document.getElementById('logout-dialog').close()">Stay Logged In</button>
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button type="submit" class="btn-primary">Logout</button>
+                </form>
+            </footer>
+        </article>
+    </dialog>
+    <dialog id="profile-dialog" class="dialog" aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
+        <article>
+            <header>
+                <h2 id="profile-dialog-title">Profile</h2>
+            </header>
+
+            <content>
+                <form class="form grid gap-6">
+            @csrf
+             <div class="grid gap-2">
+              <label for="demo-card-form-email">User Type</label>
+              <div id="select-userType♥S" class="select">
+                <button type="button" class="btn-outline justify-between font-normal w-full" id="select-userType♥S-trigger" aria-haspopup="listbox" aria-expanded="false" aria-controls="select-userType♥S-listbox">
+                  <span class="truncate"></span>
+              
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevrons-up-down-icon lucide-chevrons-up-down text-muted-foreground opacity-50 shrink-0">
+                    <path d="m7 15 5 5 5-5" />
+                    <path d="m7 9 5-5 5 5" />
+                  </svg>
+                </button>
+                <div id="select-user-type-popover" data-popover aria-hidden="true">
+                  <div role="listbox" id="select-user-type-listbox" aria-orientation="vertical" aria-labelledby="select-user-type-trigger" data-empty="No framework found.">
+                    <div role="option" data-value="user">User</div>
+                    <div role="option" data-value="admin">Admin</div>
+                  </div>
+                </div>
+                <input type="hidden" name="userType" value="{{ Auth::user()->userType }}" />
+              </div>
+
+            </div>
+            <div class="grid gap-2">
+              <label for="demo-card-form-email">Name</label>
+              <input type="text" name="name" id="demo-card-form-email" value="{{ Auth::user()->userName }}">
+            </div>
+            <div class="grid gap-2">
+              <label for="demo-card-form-email">Email</label>
+              <input type="email" name="email" id="demo-card-form-email" value="{{ Auth::user()->userEmail }}">
+            </div>
+          </form>
+            </content>
+
+            <footer>
+                <button class="btn-outline" onclick="document.getElementById('profile-dialog').close()">Back</button>
+                <button class="btn-primary" onclick="document.getElementById('profile-dialog').close()">Edit Profile</button>
+            </footer>
+        </article>
+    </dialog>
 @endsection
